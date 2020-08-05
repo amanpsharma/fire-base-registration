2429d04adefb6cb186e284f923b9b2b689fefe69
Aman Sharma <aman@excelsiorstudio.in>
2020-08-05 19:18:11 +0530

    changes in contacts.js page

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -20,12 +20,12 @@ function Contacts() {
     if (currentId === "")
       firebase.child("contacts").push(obj, (err) => {
         Swal.fire({
-          position: 'top-end',
-          icon: 'success',
-          title: 'Your work has been saved',
+          position: "top-end",
+          icon: "success",
+          title: "Your work has been saved",
           showConfirmButton: false,
-          timer: 1500
-        })
+          timer: 1500,
+        });
         if (err) console.log(err);
         else setCurrentId("");
       });
